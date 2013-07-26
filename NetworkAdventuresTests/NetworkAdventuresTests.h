aa5137c1749e69ec56d73e93e372be5bc11e5bb1
Jeff Friesen <j.friesen@tippingcanoe.com>
2013-07-26 07:23:56 -0500

    no message

---
+++ NetworkAdventuresTests/NetworkAdventuresTests.h
@@ -0,0 +1,13 @@
+//
+//  NetworkAdventuresTests.h
+//  NetworkAdventuresTests
+//
+//  Created by Jeff on 2013-07-26.
+//  Copyright (c) 2013 Tipping Canoe. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NetworkAdventuresTests : SenTestCase
+
+@end
---
+++ NetworkAdventuresTests/NetworkAdventuresTests.m
@@ -0,0 +1,32 @@
+//
+//  NetworkAdventuresTests.m
+//  NetworkAdventuresTests
+//
+//  Created by Jeff on 2013-07-26.
+//  Copyright (c) 2013 Tipping Canoe. All rights reserved.
+//
+
+#import "NetworkAdventuresTests.h"
+
+@implementation NetworkAdventuresTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NetworkAdventuresTests");
+}
+
+@end
